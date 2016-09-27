23e4ec16935aed22242dd331dbc86426725c6d80
Justin Doty <justindoty12@gmail.com>
2016-09-27 13:51:59 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+var router=require('express').Router();
+var path=require('path');
+
+router.get('/', function(req, res){
+  var createdPath=path.join(__dirname, "../public/views/index.html");
+  res.sendFile(createdPath);
+})
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+
+
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+
+app.use('/', index);
+
+
+
+
+
+
+
+
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname, 'public/views/index.html'));
+});
+
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  var port= server.address().port;
+  console.log('Listening on port ', port);
+});
